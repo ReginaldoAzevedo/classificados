bbe7f9deb6e88c4042d80ebead76fb477bf20b5d
Reginaldo Azevedo <reginaldo@raphoto.com.br>
2020-11-19 10:04:21 -0300

    Aula meus anuncios

--- README.txt
+++ README.txt
@@ -1,1 +1,2 @@
 Sistemma Classificado b7web_PHP_ZP
+Preparando para subir para o Git-Hub
---
+++ meus-anuncios.php
@@ -0,0 +1,20 @@
+<?php require 'pages/header.php';?>
+
+<div class="container-fluid">
+ <h1>Meus anuncios</h1>
+
+ <table class="table table-striped">
+ <thead>
+     <tr>
+        <th>Foto</th>
+        <th>Titulo</th>
+        <th>Valor</th>
+        <th>Ações</throw>
+     </tr>
+ </thead>
+</table>
+
+</div>
+
+
+<?php require 'pages/footer.php';?>
